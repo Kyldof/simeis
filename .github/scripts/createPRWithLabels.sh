754e01dc0ad101d3c9ae23914f030b15e9df20a8
Kyldof <kyllian.ronne@laposte.net>
2026-06-19 11:18:23 +0200

    delete sh
    because I need chekout for use it and I don't want

--- .github/scripts/createPRWithLabels.sh
+++
@@ -1,7 +0,0 @@
-#ce code à besoin de la variable d'environnement LABELS
-
-TARGETS=$(echo "$LABELS" | jq -r '.[] | select(startswith("propagate:")) | ltrimstr("propagate:")')
-
-echo "$TARGETS" | while read -r target; do
-  echo "target is $target"
-done
